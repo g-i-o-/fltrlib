f874f0414a7d7ae2efdf72c8cfc2682518b25e67
g-i-o- <aleph.omega@gmail.com>
2015-10-21 18:10:34 -0400

    committing latest changes for merging with home laptop

--- notebook/plotutils.py
+++ notebook/plotutils.py
@@ -71,11 +71,20 @@ def set_tick_labels(plot, axis, ticks, count=None):
         map(ticks, getattr(plot, 'get_'+axis+'ticks')().tolist())
     )
     
-def scatterplot_matrix(X, names=None, plotter=density_plot, hist_plotter=matplotlib.pyplot.hist, bins=None, hbins=None, tick_count=None, tick_fmt=None, rc=None, **kwargs):
+def scatterplot_matrix(X, names=None, plotter=density_plot, hist_plotter=matplotlib.pyplot.hist, bins=None, hbins=None, tick_count=None, tick_fmt=None, rc=None, showfn=None, mtype=None, axshowfn=None, **kwargs):
     """Matrix of m x m plots, where m is X.shape[2]."""
     m, n = X.shape
     if not names:
         names = map(str, range(n))
+        
+    if not showfn and mtype:
+        if mtype[0] == 'U':
+            showfn = (lambda i,j: i<=j) if mtype == 'UD' else (lambda i,j: i<j)
+        elif mtype[0] == 'D':
+            showfn = (lambda i,j: i>=j) if mtype == 'DD' else (lambda i,j: i>j)
+            
+    if not axshowfn:
+        axshowfn = lambda i, j: (i == 0 or i + 1 == n)
         
     if bins:
         try:
@@ -89,13 +98,21 @@ def scatterplot_matrix(X, names=None, plotter=density_plot, hist_plotter=matplot
     if rc:
         rcOld = matplotlib.rcParams.copy()
         matplotlib.rcParams.update(rc)
+        
+    hist_kwargs = {}
+    for k, v in kwargs.items():
+        if k[:5] == 'hist_':
+            hist_kwargs[k[5:]] = v
+            del kwargs[k]
     
     for i in range(n):
         for j in range(n):
+            if showfn and not showfn(i, j):
+                continue
             ni, nj = names[i], names[j]
             if i == j:
                 sp = matplotlib.pyplot.subplot(n, n, i*n + j + 1)
-                hist_plotter(X[:,j], bins=hbins[i] if hbins else 150)
+                hist_plotter(X[:,j], bins=hbins[i] if hbins else 150, **hist_kwargs)
                 set_plot_axis(sp, 'y', visible=False)
             else:
                 sp = matplotlib.pyplot.subplot(n, n, i*n + j + 1)
@@ -111,7 +128,7 @@ def scatterplot_matrix(X, names=None, plotter=density_plot, hist_plotter=matplot
                 else:
                     set_plot_axis(sp, 'y', visible=False)
 
-            if i == 0 or i + 1 == n:
+            if axshowfn(i, j):
                 pos = "top" if i == 0 else "bottom"
                 set_plot_axis(sp, 'x', visible=True, tick_position=pos, label=nj, label_position=pos)
                 if tick_fmt:
@@ -195,10 +212,14 @@ def bin_shift(X, bins, step):
     return X + step * (X.max() - X.min()) / bins
 
 
-def violin_density_plot(X, bins, vbins=100, cmap=matplotlib.cm.gray,**kwargs):
+def violin_density_plot(X, bins, vbins=100, cmap=matplotlib.cm.gray, log=True, **kwargs):
     """Violin plot of 1-d data, coupled with density information."""
     H = numpy.histogram(X, bins=bins)[0]
-    H = numpy.log(H + 1)
+    if log:
+        if log is True:
+            H = numpy.log(H + 1)
+        else:
+            H = numpy.log(H + 1) / numpy.log(log)
     H2 =numpy.zeros((vbins, bins), dtype=H.dtype)
     maxh = H.max()
     b2 = vbins / 2
@@ -225,8 +246,12 @@ def density_contour_plot(X, Y, *args, **kwargs):
     Y = bin_shift(Y, bins[1], .5)
     contour_plot(X, Y, *args, cmap=matplotlib.cm.jet, **kwargs),
 
-def violin_contour_plot(X, bins):
-    H2 = violin_density_plot(X, bins, interpolation="nearest", cmap=matplotlib.cm.gist_earth)
+def violin_contour_plot(X, bins, **kwargs):
+    if 'interpolation' not in kwargs:
+        kwargs['interpolation'] = "nearest"
+    if 'cmap' not in kwargs:
+        kwargs['cmap'] = matplotlib.cm.gist_earth
+    H2 = violin_density_plot(X, bins, **kwargs)
     Hx = numpy.linspace(X.min(), X.max(), bins)
     Hy = numpy.arange(-H2.shape[0]/2, H2.shape[0]/2)
     matplotlib.pyplot.contour(Hx, Hy, H2, origin='lower', aspect='auto')
